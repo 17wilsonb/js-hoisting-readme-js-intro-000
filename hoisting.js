d6c4e2a229bf558bada980c8adc925fddb5d31d8
Brett Wilson <wilson.3458@osu.edu>
2020-05-04 08:26:50 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -18,8 +18,8 @@ function crazy() {
 function sayMyName() {
   var name = "Cricky";
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
   sayMy();
 }
